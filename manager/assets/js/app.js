859b12a16acb0c1035c2bf2e845c400abd9a1461
Dmytro Orikhovskyi <d>
2020-03-27 21:54:50 +0200

    centrifugo auth

--- manager/assets/js/app.js
+++ manager/assets/js/app.js
@@ -6,22 +6,13 @@ require('@coreui/coreui');
 const Centrifuge = require('centrifuge');
 const toastr = require('toastr');
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     let url = document.querySelector('meta[name=centrifugo-url]').getAttribute('content');
-//     let user = document.querySelector('meta[name=centrifugo-user]').getAttribute('content');
-//     let token = document.querySelector('meta[name=centrifugo-token]').getAttribute('content');
-//     let centrifuge = new Centrifuge(url);
-//     centrifuge.setToken(token);
-//     centrifuge.subscribe('alerts#' + user, function (message) {
-//         toastr.info(message.data.message);
-//     });
-//     centrifuge.connect();
-// });
-
 document.addEventListener('DOMContentLoaded', function () {
     let url = document.querySelector('meta[name=centrifugo-url]').getAttribute('content');
-    const centrifuge = new Centrifuge(url);
-    centrifuge.subscribe('alerts', function (message) {
+    let user = document.querySelector('meta[name=centrifugo-user]').getAttribute('content');
+    let token = document.querySelector('meta[name=centrifugo-token]').getAttribute('content');
+    let centrifuge = new Centrifuge(url);
+    centrifuge.setToken(token);
+    centrifuge.subscribe('alerts#' + user, function (message) {
         toastr.info(message.data.message);
     });
     centrifuge.connect();
