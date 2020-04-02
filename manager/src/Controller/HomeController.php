6f6944f5609b6540e6737bec64f5cc91262cd255
Dmytro Orikhovskyi <d>
2020-04-02 22:24:37 +0300

    swagger

--- manager/src/Controller/HomeController.php
+++ manager/src/Controller/HomeController.php
@@ -1,10 +1,14 @@
 <?php
 namespace App\Controller;
 
+use App\Annotation\RequiresUserCredits;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @RequiresUserCredits()
+ */
 class HomeController extends AbstractController
 {
     /**
